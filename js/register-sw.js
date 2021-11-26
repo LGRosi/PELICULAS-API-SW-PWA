aaf5600fa21a98689682c93fb94a225c0ba2e4a9
LGRosi <lrosi1996@gmail.com>
2021-11-26 18:23:00 -0300

    register-sw

--- js/register-sw.js
+++ js/register-sw.js
@@ -1,6 +1,6 @@
 // Chequeo si el browser soporta Service Worker
 if ('serviceWorker' in navigator){
-    navigator.serviceWorker.register("service-worker.js").then((message) => {
+    navigator.serviceWorker.register("../service-worker.js").then((message) => {
       console.log("Service Worker esta listo!");
     });
   } else {
