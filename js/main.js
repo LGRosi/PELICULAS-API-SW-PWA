a51698fb63f934bc17f81d66ee10acab39c285a1
LGRosi <lrosi1996@gmail.com>
2021-11-26 19:18:28 -0300

    actualización del color del titulo, offline/online

--- js/main.js
+++ js/main.js
@@ -1,9 +1,11 @@
 window.addEventListener('offline', (event) => {
     console.log('Estoy offline!');
+    document.querySelector('#idSw').classList.add('offline');
 });
 
 window.addEventListener('online', (event) => {
 console.log('Estoy online! Puedo navegar!');
+document.querySelector('#idSw').classList.add('online');
 });
 
 if (!navigator.onLine) {
